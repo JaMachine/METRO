bb301c79904884e6e2a00f291ba8fba3c9bb8966
JaMachine <fenia5500@meta.ua>
2017-04-23 19:15:16 +0300

    # WARNING: head commit changed in the meantime
    version0.01

--- M__E__T__R__O/src/Main/Main.java
+++ M__E__T__R__O/src/Main/Main.java
@@ -5,5 +5,7 @@ import Metro.*;
 public class Main {
 
 	Station s = new Station("zzk", 77);
+	
+
 
 }
--- M__E__T__R__O/src/Metro/Station.java
+++ M__E__T__R__O/src/Metro/Station.java
@@ -1,15 +1,15 @@
 package Metro;
 
-
 public class Station {
-	
+
 	String name;
 	int id;
-	
+	long cashbox = 0;
+
 	public Station(String name, int id) {
 		this.name = name;
 		this.id = id;
+
 	}
-	
 
 }
