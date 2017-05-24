57edde7f40c20f914194d8c47892cd99c17523a7
JaMachine <fenia5500@meta.ua>
2017-05-24 11:33:35 +0300

    ClassWork minor fix

--- M__E__T__R__O/src/Metro/Depot.java
+++ M__E__T__R__O/src/Metro/Depot.java
@@ -1,6 +1,8 @@
 package Metro;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Depot {
 
@@ -9,6 +11,28 @@ public class Depot {
 		for (int i = 0; i < 10; i++) {
 			trains.add(new Train());
 		}
+		
+
+		Comparator<Driver> copmarator = new Comparator<Driver>() {
+
+			@Override
+			public int compare(Driver o1, Driver o2) {
+				if (o1.getExp() > o2.getExp()) {
+					return -1;
+				}
+				if (o1.getExp() < o2.getExp()) {
+					return 1;
+				}
+				return 0;
+			}
+
+		};
+		PriorityQueue<Driver> drivers = new PriorityQueue<>(copmarator);
+
+		for (int i = 0; i < 5; i++) {
+			drivers.add(new Driver());
+		}
+		System.out.println(drivers);
 	}
 
 	// ArrayList <Wagon>wagons = new ArrayList<>();
